a1fb33363cce2b48c97d009fe61eda62d9d19bbb
Yan Klyuzhin <klyuzhin@uchi.ru>
2020-11-26 00:48:40 +0300

    added rendering menu & filters templates

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import {createTripInfoTemplate} from './view/trip-info';
+import {createMenuTemplate} from './view/menu';
+import {createFiltersTemplate} from './view/filters';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -7,5 +9,11 @@ const render = (container, template, place) => {
 const siteBodyElement = document.querySelector(`.page-body`);
 const siteHeaderElement = siteBodyElement.querySelector(`.page-header`);
 const tripMainElement = siteHeaderElement.querySelector(`.trip-main`);
+const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
+const tripControlsHeaderElements = tripControlsElement.querySelectorAll(`h2`);
 
+const menuAndFilters = [createMenuTemplate(), createFiltersTemplate()];
 render(tripMainElement, createTripInfoTemplate(), `afterbegin`);
+tripControlsHeaderElements.forEach((h2, ind) => {
+  render(h2, menuAndFilters[ind], `afterend`);
+});
