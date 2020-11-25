9664db15b520a535464650ffacb5110449b7a179
Yan Klyuzhin <klyuzhin@uchi.ru>
2020-11-26 01:58:20 +0300

    added rendering sorting element

--- src/main.js
+++ src/main.js
@@ -1,19 +1,27 @@
 import {createTripInfoTemplate} from './view/trip-info';
 import {createMenuTemplate} from './view/menu';
 import {createFiltersTemplate} from './view/filters';
+import { createSortTemplate } from './view/sort';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const siteBodyElement = document.querySelector(`.page-body`);
+
 const siteHeaderElement = siteBodyElement.querySelector(`.page-header`);
 const tripMainElement = siteHeaderElement.querySelector(`.trip-main`);
 const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
 const tripControlsHeaderElements = tripControlsElement.querySelectorAll(`h2`);
+
+const siteMainElement = siteBodyElement.querySelector(`.page-main`);
+
+const tripEventsElement = siteMainElement.querySelector(`.trip-events`);
+const tripEventsHeaderElement = tripEventsElement.querySelector(`h2`);
 
 const menuAndFilters = [createMenuTemplate(), createFiltersTemplate()];
 render(tripMainElement, createTripInfoTemplate(), `afterbegin`);
 tripControlsHeaderElements.forEach((h2, ind) => {
   render(h2, menuAndFilters[ind], `afterend`);
 });
+render(tripEventsHeaderElement, createSortTemplate(), `afterend`);
