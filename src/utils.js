c0db6c8c0ac1d728c38299c0a4307c2be19e8320
Yan Klyuzhin <klyuzhin@uchi.ru>
2020-12-08 00:20:11 +0300

    made point template function display particular point object

---
+++ src/utils.js
@@ -0,0 +1,25 @@
+export const getDuration = (start, end) => {
+  const daysDuration = end.diff(start, `day`);
+  const hoursDuration = end.subtract(daysDuration, `day`).diff(start, `hour`);
+  const minutesDuration = end.subtract(daysDuration, `day`).subtract(hoursDuration, `hour`).diff(start, `minute`);
+  let duration = `${minutesDuration}M`;
+  if (!minutesDuration) {
+    duration = `0`.concat(duration);
+  }
+  if (hoursDuration) {
+    duration = `${hoursDuration}H `.concat(duration);
+    if (hoursDuration < 10) {
+      duration = `0`.concat(duration);
+    }
+  }
+  if (daysDuration) {
+    if (!hoursDuration) {
+      duration = `00H `.concat(duration);
+    }
+    duration = `${daysDuration}D `.concat(duration);
+    if (daysDuration < 10) {
+      duration = `0`.concat(duration);
+    }
+  }
+  return duration;
+};
