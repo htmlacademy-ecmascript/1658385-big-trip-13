cf42c65537f2e4e61d2d8e4d435313e995052a19
Yan Klyuzhin <klyuzhin@uchi.ru>
2021-01-22 04:38:32 +0300

    refactored utils & render methods

---
+++ src/utils/render.js
@@ -0,0 +1,50 @@
+import AbstractView from '../view/abstract';
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstElementChild;
+};
+
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+export const render = (container, child, place) => {
+  if (container instanceof AbstractView) {
+    container = container.getElement();
+  }
+
+  if (child instanceof AbstractView) {
+    child = child.getElement();
+  }
+
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(child);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(child);
+      break;
+  }
+};
+
+export const replace = (newChild, oldChild) => {
+  if (newChild instanceof AbstractView) {
+    newChild = newChild.getElement();
+  }
+
+  if (oldChild instanceof AbstractView) {
+    oldChild = oldChild.getElement();
+  }
+
+  const parent = oldChild.parentElement;
+
+  if (parent === null || newChild === null || oldChild === null) {
+    throw new Error(`Error trying to replace null or to null`);
+  }
+
+  parent.replaceChild(newChild, oldChild);
+};
